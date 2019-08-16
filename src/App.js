01d65669a036e4a2704f010d35fc656d5239601d
Tadeu Tupinambá <tadeu.tupiz@gmail.com>
2019-08-15 22:46:38 -0300

    🔧 prettier + eslint

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {};
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header" />
+            </div>
+        );
+    }
 }
 
 export default App;
